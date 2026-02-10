df0d2a6ce0bc99ef0f84a5e175189c6dfac113ce
Ersya Hasby Satria <hasbysatria54@gmail.com>
2026-02-11 06:10:16 +0700

    Update main.dart

--- TASK0/lib/main.dart
+++ TASK0/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Logbook: SRP Version'),
     );
   }
 }
@@ -48,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text('Total Hitungan:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
